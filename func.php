be011cbb48c56a76db3f77427344198cafb71ade
kabayan349 <67649284+kabayan349@users.noreply.github.com>
2020-07-21 14:38:32 +0700

    dsgjg

--- func.php
+++ func.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.50.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.176412,106.826915";
+$header[] = "X-Location: -6.908091,107.607105";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
